cc5075599c142cc60140d47276b332017857f271
Haikal Fadhilah <haikalfadhilah34@gmail.com>
2025-01-29 14:06:46 +0700

    adding pitch and roll lerp for smoothing movement

--- Course 4 Galaxy Strikes/Assets/Scripts/PlayerMovement.cs
+++ Course 4 Galaxy Strikes/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
     [SerializeField] float YClampRange = 35f;
 
     [SerializeField] float controlRollFactor = 20f; 
+    [SerializeField] float controlPitchFactor = 0;
+    [SerializeField] float rotationSpeed = 10f;
     Vector2 movement;
     void Start()
     {
@@ -42,11 +44,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
 
+        
+
     }
     void ProcessRotation()
     {
-        Quaternion targetRotation = Quaternion.Euler(0f, 0f, -controlRollFactor * movement.x);
-        transform.localRotation = targetRotation;
-
+        Quaternion targetRotation = Quaternion.Euler(-controlPitchFactor * movement.y, 0f, -controlRollFactor * movement.x);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
