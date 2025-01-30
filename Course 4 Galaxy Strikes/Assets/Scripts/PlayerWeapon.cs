998f11f09db08ab2384f844a75cce03d406759aa
Haikal Fadhilah <haikalfadhilah34@gmail.com>
2025-01-30 14:53:15 +0700

    Adding Crosshair logic and deleting Cursor on main focussed game

--- Course 4 Galaxy Strikes/Assets/Scripts/PlayerWeapon.cs
+++ Course 4 Galaxy Strikes/Assets/Scripts/PlayerWeapon.cs
@@ -4,10 +4,18 @@ using UnityEngine.InputSystem;
 public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] GameObject[] lasers;
+    [SerializeField] RectTransform crosshair;
     bool isFiring = false;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
     private void Update() 
     {
         processFiring();
+        MoveCrosshair();
+    
     }
     public void OnFire(InputValue value)
     {
@@ -22,5 +30,9 @@ public class PlayerWeapon : MonoBehaviour
             emmissionModule.enabled = isFiring;     
         }
         
+    }
+    void MoveCrosshair()
+    {
+        crosshair.position = Input.mousePosition;
     }
 }
