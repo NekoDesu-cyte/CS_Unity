d0826326e6d9b541293d4bd54ff39f59388127f1
Haikal Fadhilah <haikalfadhilah34@gmail.com>
2025-01-30 15:51:28 +0700

    Adding Collision handler and fixing some crosshair things

--- Course 4 Galaxy Strikes/Assets/Scripts/PlayerWeapon.cs
+++ Course 4 Galaxy Strikes/Assets/Scripts/PlayerWeapon.cs
@@ -5,6 +5,8 @@ public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] GameObject[] lasers;
     [SerializeField] RectTransform crosshair;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float targetDistance = 100f;
     bool isFiring = false;
 
     void Start()
@@ -15,6 +17,8 @@ public class PlayerWeapon : MonoBehaviour
     {
         processFiring();
         MoveCrosshair();
+        MoveTargetPoint();
+        AimLaser();
     
     }
     public void OnFire(InputValue value)
@@ -34,5 +38,19 @@ public class PlayerWeapon : MonoBehaviour
     void MoveCrosshair()
     {
         crosshair.position = Input.mousePosition;
+    }
+    void MoveTargetPoint()
+    {
+        Vector3 targetPointPoisition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, targetDistance); 
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPoisition);
+    }
+    void AimLaser()
+    {
+        foreach (var laser in lasers)
+        {
+            Vector3 fireDirection = targetPoint.position - this.transform.position;
+            Quaternion rotationToTarget = Quaternion.LookRotation(fireDirection);
+            laser.transform.rotation = rotationToTarget;
+        }
     }
 }
