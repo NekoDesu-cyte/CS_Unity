13b587fe6871519c1263adb798c95c18de52571d
Haikal Fadhilah <haikalfadhilah34@gmail.com>
2025-01-31 21:48:45 +0700

    Few

--- Course 4 Galaxy Strikes/Assets/Scripts/DialogLine.cs
+++ Course 4 Galaxy Strikes/Assets/Scripts/DialogLine.cs
@@ -1,14 +1,15 @@
-using TMPro;
+    using TMPro;
 using UnityEngine;
 
 public class DialogLine : MonoBehaviour
 {
-    [SerializeField] TMP_Text dialogText;
     [SerializeField] string[] timelineTextLines;
+    [SerializeField] TMP_Text dialogText;
+ 
 
     int currentLine = 0;
 
-    void NextDialogLine()
+    public void NextDialogLine()
     {
         currentLine++;
         dialogText.text = timelineTextLines[currentLine];
