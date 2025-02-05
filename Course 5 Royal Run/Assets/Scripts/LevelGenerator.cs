d3f2874baedfe639dd2bf55b3347d116f92ba353
Haikal Fadhilah <haikalfadhilah34@gmail.com>
2025-02-05 17:22:26 +0700

    Adding The Prefabs moving backward using Loop

--- Course 5 Royal Run/Assets/Scripts/LevelGenerator.cs
+++ Course 5 Royal Run/Assets/Scripts/LevelGenerator.cs
@@ -6,25 +6,52 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] GameObject chunkPrefab;
     [SerializeField] int startingChunkAmmount = 12;
     [SerializeField] Transform chunkParent;
-    [SerializeField] float chunkLenght = 0f;
+    [SerializeField] float chunkLenght = 10f;
+    [SerializeField] float moveSpeed  = 8f;
 
-    private void Start() 
+    GameObject[] chunks = new GameObject[12];
+
+    private void Start()
+    {
+        SpawnChunks();
+    }
+    private void Update() {
+        moveCHunk();
+    }
+
+    private void SpawnChunks()
     {
         for (int i = 0; i < startingChunkAmmount; i++)
         {
-            float spawnPositionZ;
-
-            if (i == 0)
-            {
-                spawnPositionZ = transform.position.z;
-            }
-            else
-            {
-                spawnPositionZ = transform.position.z + (i * chunkLenght);
-            }
+            float spawnPositionZ = CalculateSpawnPositionZ(i);
 
             Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
-            Instantiate(chunkPrefab, chunkSpawnPos, Quaternion.identity, chunkParent); 
+            GameObject newChunk = Instantiate(chunkPrefab, chunkSpawnPos, Quaternion.identity, chunkParent);
+
+            chunks[i] = newChunk;
+        }
+    }
+
+    private float CalculateSpawnPositionZ(int i)
+    {
+        float spawnPositionZ;
+
+        if (i == 0)
+        {
+            spawnPositionZ = transform.position.z;
+        }
+        else
+        {
+            spawnPositionZ = transform.position.z + (i * chunkLenght);
+        }
+
+        return spawnPositionZ;
+    }
+    void moveCHunk()
+    {
+        for (int i = 0; i < chunks.Length; i++)
+        {
+            chunks[i].transform.Translate(-transform.forward * (moveSpeed * Time.deltaTime));
         }
     }
 }
