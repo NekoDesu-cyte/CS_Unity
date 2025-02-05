555e6ea3653411d7b1b5ef5f0a36ff15d1774f65
Haikal Fadhilah <haikalfadhilah34@gmail.com>
2025-02-05 16:15:29 +0700

    Adding Level generator,which is generator to plain with serilizefield

--- Course 5 Royal Run/Assets/Scripts/LevelGenerator.cs
+++ Course 5 Royal Run/Assets/Scripts/LevelGenerator.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] GameObject chunkPrefab;
+    [SerializeField] int startingChunkAmmount = 12;
+    [SerializeField] Transform chunkParent;
+    [SerializeField] float chunkLenght = 0f;
 
-    private void Start() {
-        Instantiate(chunkPrefab, transform.position, quaternion.identity);
+    private void Start() 
+    {
+        for (int i = 0; i < startingChunkAmmount; i++)
+        {
+            float spawnPositionZ;
+
+            if (i == 0)
+            {
+                spawnPositionZ = transform.position.z;
+            }
+            else
+            {
+                spawnPositionZ = transform.position.z + (i * chunkLenght);
+            }
+
+            Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+            Instantiate(chunkPrefab, chunkSpawnPos, Quaternion.identity, chunkParent); 
+        }
     }
 }
