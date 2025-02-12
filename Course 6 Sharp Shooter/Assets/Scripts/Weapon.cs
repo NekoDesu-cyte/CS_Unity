f249c5eda7a09cbe4dfc7efb20ae0e9c7ce541ac
Haikal Fadhilah <haikalfadhilah34@gmail.com>
2025-02-12 14:32:45 +0700

    Adding new Lines for animation recoil

--- Course 6 Sharp Shooter/Assets/Scripts/Weapon.cs
+++ Course 6 Sharp Shooter/Assets/Scripts/Weapon.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damageAmount = 1;
     StarterAssetsInputs starterAssetsInputs;
+
+    const string SHOOT_STRING = "Shoot";
 
     void Awake()
     {
@@ -22,6 +25,8 @@ public class Weapon : MonoBehaviour
         if (!starterAssetsInputs.shoot) return;
 
         muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
 
         RaycastHit hit;
 
@@ -30,7 +35,6 @@ public class Weapon : MonoBehaviour
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(damageAmount);
 
-            starterAssetsInputs.ShootInput(false);   
         }
     }
 }
