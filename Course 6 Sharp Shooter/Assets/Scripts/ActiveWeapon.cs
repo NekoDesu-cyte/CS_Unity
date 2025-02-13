bc12c52755e11b1b4ac4df45c7f4c7f9c4049e32
Haikal Fadhilah <haikalfadhilah34@gmail.com>
2025-02-13 16:50:14 +0700

    adding new lines for slowing down the rotation speed + adding new lines for zooming to ZoomFOV and to Default Zoom

--- Course 6 Sharp Shooter/Assets/Scripts/ActiveWeapon.cs
+++ Course 6 Sharp Shooter/Assets/Scripts/ActiveWeapon.cs
@@ -1,22 +1,30 @@
+using Cinemachine;
 using StarterAssets;
 using UnityEngine;
 
 public class ActiveWeapon : MonoBehaviour
 {
     [SerializeField] WeaponSO weaponSO;
+    [SerializeField] CinemachineVirtualCamera playerFollowCamera;
+    [SerializeField] GameObject zoomVignet;
     Animator animator;
     StarterAssetsInputs starterAssetsInputs;
+    FirstPersonController firstPersonController;
     Weapon currentWeapon;
 
     const string SHOOT_STRING = "Shoot";
 
     float timeSinceLastShot = 0f;
-    
+    float defaultFOV;
+    float defualtRotationSPeed; 
 
     void Awake()
     {
         starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+        firstPersonController = GetComponentInParent<FirstPersonController>();
         animator = GetComponent<Animator>();
+        defaultFOV = playerFollowCamera.m_Lens.FieldOfView;
+        defualtRotationSPeed = firstPersonController.RotationSpeed;
     }
     void Start()
     {
@@ -67,11 +75,16 @@ public class ActiveWeapon : MonoBehaviour
 
         if (starterAssetsInputs.zoom)
         {
-            Debug.Log ("Zooming in");
+            playerFollowCamera.m_Lens.FieldOfView = weaponSO.ZoomAmmount;
+            zoomVignet.SetActive(true);
+            firstPersonController.ChangeRotationSPeed(weaponSO.ZoomRotationSpeed);
+
         }
         else 
         {
-            Debug.Log ("not zooming in");
+            playerFollowCamera.m_Lens.FieldOfView = defaultFOV;
+            zoomVignet.SetActive(false);
+            firstPersonController.ChangeRotationSPeed(defualtRotationSPeed);
         }
     }
  
--- Course 6 Sharp Shooter/Assets/Scripts/WeaponSO.cs
+++ Course 6 Sharp Shooter/Assets/Scripts/WeaponSO.cs
@@ -9,4 +9,6 @@ public class WeaponSO : ScriptableObject
     public GameObject HitVFXPrefab;
     public bool IsAutomatic = false;
     public bool CanZoom = false;
+    public float ZoomAmmount = 10f;
+    public float ZoomRotationSpeed = .3f;
 }
