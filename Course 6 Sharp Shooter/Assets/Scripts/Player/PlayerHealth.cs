ff6e112f4bdf89d752a01236fecde0877fa1327c
Haikal Fadhilah <haikalfadhilah34@gmail.com>
2025-02-14 14:36:05 +0700

    adding new health bar system to the script and can be adjusted and has range limit to adjust the health bar

--- Course 6 Sharp Shooter/Assets/Scripts/Player/PlayerHealth.cs
+++ Course 6 Sharp Shooter/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,23 +1,47 @@
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] int startingHealth = 3;
+    [Range(1, 10)] 
+    [SerializeField] int startingHealth = 5;
+    [SerializeField] CinemachineVirtualCamera deathVirtualCamera;   
+    [SerializeField] Transform weaponCamera;
+    [SerializeField] Image[] shieldBars;
 
     int currentHealth;
+    int gameOverVirtualCameraPriority = 20;
 
     void Awake()
     {
         currentHealth = startingHealth;
+        AdjustShieldUI();
     }
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
-        Debug.Log(amount + "damage taken");
+        AdjustShieldUI();
 
         if (currentHealth <= 0)
         {
+            weaponCamera.parent = null;
+            deathVirtualCamera.Priority = 20;
             Destroy(this.gameObject);
+        }
+    }
+    void AdjustShieldUI()
+    {
+        for (int i = 0; i < shieldBars.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                shieldBars[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                shieldBars[i].gameObject.SetActive(false);
+            }
         }
     }
 }
